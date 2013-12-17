d364a718aa8b6c9582c211ec46637fc1a0c1e4e4
Alex Wilkinson <alex.wilkinson@me.com>
2013-12-17 00:37:56 -0500

    now filters for stop words and hyphens; changed method to strip_term

--- data-reconciler 3.rb
+++ data-reconciler 3.rb
@@ -6,46 +6,49 @@ require 'set'
 class Reconciler
 	@@iptc_data = Hash.new
 	@@stop_words = Set["a", "about", "across", "after", "all", "also", "an", "and", "as", "at", "be", "been", "by", "can", "for", "from", "have", "in", "into", "its", "of", "off", "on", "or", "since", "that", "the", "their", "to", "were", "where", "which", "while", "who", "with"]
+	
 	def self.iptc_load
 		CSV.foreach('iptc_sample.csv') do |row|
-			@@dump = Array.new(row)
-			iptc_term = @@dump[7].downcase
-			Reconciler.stem_term(iptc_term)
-			@@iptc_data[iptc_term] = @@term_stemmed
-			puts @@iptc_data
+			dump = Array.new(row)
+			iptc_term = dump[7].downcase
+			Reconciler.strip_term(iptc_term)
+			@@iptc_data[iptc_term] = @@term_stripped
 		end
 	end
 
-	def self.stem_term(term)
+	def self.strip_term(term)
 		split_term = term.split(' ')
 		@@split_term_stemmed = []
 		split_term.each do |word|
+			if @@stop_words.include?(word)
+				next
+			end
 			if word.include?('-')
 				word.delete!('-')
 			end
 			@@split_term_stemmed.push(word.stem)
 		end
-		@@term_stemmed = @@split_term_stemmed.join(' ')
+		@@term_stripped = @@split_term_stemmed.join(' ')
 	end
-
 
 	def self.iptc_compare(dbp_term)
 		if @@iptc_data.has_key?(dbp_term)
 			puts "#{dbp_term} matched!"
-			# CSV.open('matches.csv', mode = "a+") do |file|
-			# 	file << [dbp_term]
-			# end
+			CSV.open('matches2.csv', mode = "a+") { |file| file << [dbp_term] }
 		else
-			Reconciler.stem_term(dbp_term)
-			dbp_term_stemmed = @@term_stemmed
+			Reconciler.strip_term(dbp_term)
+			dbp_term_stripped = @@term_stripped
 			jarrow = FuzzyStringMatch::JaroWinkler.create( :native )
-			@@iptc_data.each do |iptc_term, iptc_term_stemmed|
-				distance = jarrow.getDistance(iptc_term_stemmed,dbp_term_stemmed)
-				if distance > 0.9 && iptc_term_stemmed.split(" ").length == dbp_term_stemmed.split(" ").length
-					puts "IPTC: #{iptc_term} (#{iptc_term_stemmed}), DBPEDIA: #{dbp_term} (#{dbp_term_stemmed}): #{distance}"
-					# CSV.open('matches.csv', mode = "a+") do |file|
-					# 	file << [dbp_term]
-					# end
+			@@iptc_data.each do |iptc_term, iptc_term_stripped|
+				distance = jarrow.getDistance(iptc_term_stripped, dbp_term_stripped)
+				if distance > 0.9
+					if dbp_term_stripped.split(' ').all? {|word| iptc_term_stripped.split(' ').include?(word)} == true && iptc_term_stripped.split(' ').all? {|word| dbp_term_stripped.split(' ').include?(word)}
+						puts "IPTC: #{iptc_term} (#{iptc_term_stripped}), DBPEDIA: #{dbp_term} (#{dbp_term_stripped}): #{distance}"
+						CSV.open('matches2.csv', mode = "a+") { |file| file << [dbp_term, iptc_term] }
+					else
+						puts "POSSIBLE MATCH - IPTC: #{iptc_term} (#{iptc_term_stripped}), DBPEDIA: #{dbp_term} (#{dbp_term_stripped}): #{distance}"
+						CSV.open('possible_matches.csv', mode = "a+") { |file| file << [dbp_term, iptc_term] }
+					end
 				end
 			end
 		end
@@ -77,7 +80,7 @@ puts "Program Duration: #{duration}" # Takes about 38 minutes right now
 
 
 # this will check to see if one of the strings contains words not in the other one
-# db_string.split(' ').all? {|word| iptc_string.split(' ').include?(word)} == false || iptc_string.split(' ').all? {|word| db_string.split(' ').include?(word)} == false
+# dbp_term_stripped.split(' ').all? {|word| iptc_term_stripped.split(' ').include?(word)} == false || iptc_term_stripped.split(' ').all? {|word| dbp_term_stripped.split(' ').include?(word)} == false
 
 
 # put each term in a set and then check if they include stop words, if included then delete
