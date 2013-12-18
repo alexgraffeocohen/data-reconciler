d37407d5c125e4d202d5e92eecb5d4cf52dec2bf
Alex Wilkinson <alex.wilkinson@me.com>
2013-12-18 11:13:28 -0500

    renamed methods, loading dbp_data into hash

--- data-reconciler 3.rb
+++ data-reconciler 3.rb
@@ -7,6 +7,7 @@ class Reconciler
 	
 	def initialize
 		@iptc_data = Hash.new
+		@dbp_data = Hash.new
 		@stop_words = Set["a", "about", "across", "after", "all", "also", "an", "and", "as", "at", "be", "been", "by", "can", "for", "from", "have", "in", "into", "its", "of", "off", "on", "or", "since", "that", "the", "their", "to", "were", "where", "which", "while", "who", "with"]
 		@matched_terms = []
 		@special_characters_delete = ["-", ",", "(", ")", ":", "&", "'", "~"]
@@ -15,11 +16,19 @@ class Reconciler
 		@possibles_csv = CSV.open('possible_matches.csv', mode = "a+")
 	end
 
-	def iptc_load
+	def load_iptc
 		CSV.foreach('iptc_sample.csv') do |row|
 			dump = Array.new(row)
 			iptc_term = dump[7].downcase
 			@iptc_data[iptc_term] = self.strip_term(iptc_term)
+		end
+	end
+
+	def load_dbp
+		CSV.foreach('dbpedia_sample.csv') do |row|
+			dump = Array.new(row)
+			dbp_term = dump[1].downcase
+			@dbp_data[dbp_term] = self.strip_term(dbp_term)
 		end
 	end
 
@@ -43,27 +52,28 @@ class Reconciler
 		@split_term_stemmed.join(' ')
 	end
 
-	def iptc_compare(dbp_term)
-		if @iptc_data.has_key?(dbp_term)
-			puts "#{dbp_term} matched!"
-			@matched_terms.push(dbp_term)
-			@matches_csv << [dbp_term]  # open the files at the beginning and close them at the end
-		else
-			dbp_term_stripped = self.strip_term(dbp_term)
-			@iptc_data.each do |iptc_term, iptc_term_stripped|
-				if @matched_terms.include?(iptc_term)
-					next
-				end
-				dbp_term_set = Set.new(dbp_term_stripped.split(' '))
-				iptc_term_set = Set.new(iptc_term_stripped.split(' '))
-				dbp_in_iptc = dbp_term_set.subset? iptc_term_set
-				iptc_in_dbp = iptc_term_set.subset? dbp_term_set
-				if dbp_term_set.length == iptc_term_set.length && dbp_in_iptc && iptc_in_dbp
-					puts "MATCH - DBPEDIA: #{dbp_term} (#{dbp_term_stripped}), IPTC: #{iptc_term} (#{iptc_term_stripped})"
-					@matches_csv << [dbp_term, iptc_term] 
-				elsif @jarrow.getDistance(iptc_term_stripped, dbp_term_stripped) > 0.90
-					puts "POSSIBLE MATCH - DBPEDIA: #{dbp_term} (#{dbp_term_stripped}), IPTC: #{iptc_term} (#{iptc_term_stripped})"
-					@possibles_csv << [dbp_term, iptc_term]
+	def compare
+		@dbp_data.each do |dbp_term, dbp_term_stripped|
+			if @iptc_data.has_key?(dbp_term)
+				puts "#{dbp_term} matched!"
+				@matched_terms.push(dbp_term)
+				@matches_csv << [dbp_term]  # open the files at the beginning and close them at the end
+			else
+				@iptc_data.each do |iptc_term, iptc_term_stripped|
+					if @matched_terms.include?(iptc_term)
+						next
+					end
+					dbp_term_set = Set.new(dbp_term_stripped.split(' '))
+					iptc_term_set = Set.new(iptc_term_stripped.split(' '))
+					dbp_in_iptc = dbp_term_set.subset? iptc_term_set
+					iptc_in_dbp = iptc_term_set.subset? dbp_term_set
+					if dbp_term_set.length == iptc_term_set.length && dbp_in_iptc && iptc_in_dbp
+						puts "MATCH - DBPEDIA: #{dbp_term} (#{dbp_term_stripped}), IPTC: #{iptc_term} (#{iptc_term_stripped})"
+						@matches_csv << [dbp_term, iptc_term] 
+					elsif @jarrow.getDistance(iptc_term_stripped, dbp_term_stripped) > 0.90
+						puts "POSSIBLE MATCH - DBPEDIA: #{dbp_term} (#{dbp_term_stripped}), IPTC: #{iptc_term} (#{iptc_term_stripped})"
+						@possibles_csv << [dbp_term, iptc_term]
+					end
 				end
 			end
 		end
@@ -76,17 +86,12 @@ class Reconciler
 
 end
 
-reconcile = Reconciler.new
-reconcile.iptc_load
-
 start_time = Time.now
 
-CSV.foreach('dbpedia_sample.csv') do |row|
-	dbp_row = Array.new(row)
-	dbp_term = dbp_row[1].downcase
-	reconcile.iptc_compare(dbp_term)
-end
-
+reconcile = Reconciler.new
+reconcile.load_iptc
+reconcile.load_dbp
+reconcile.compare
 reconcile.close_files
 
 end_time = Time.now
